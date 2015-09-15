7bb74c9c0564776dfc7b225258ca484ca744d565
cedavidyang <ynyang1988@yahoo.com>
2015-09-15 02:05:17 -0400

    shear rc w/o evidence

--- evidence/evidence.py
+++ evidence/evidence.py
@@ -31,11 +31,11 @@ evidence_dict = {'iniStat': np.copy(evidence_list),
 #evidence_dict['crkStat'][service_time==NO_CRK_YR] = False
 #evidence_dict['crkStat'][service_time==IS_CRK_YR] = True
 
-# condition state evidence
-evidence_dict['conditionState'][service_time<=CS1_TO_CS2] = 1
-evidence_dict['conditionState'][np.logical_and(service_time>CS1_TO_CS2, service_time<=CS2_TO_CS3)] = 2
-evidence_dict['conditionState'][np.logical_and(service_time>CS2_TO_CS3, service_time<=CS3_TO_CS4)] = 3
-evidence_dict['conditionState'][np.logical_and(service_time>CS3_TO_CS4, service_time<=EVIDENCE_END_YR)] = 4
+## condition state evidence
+#evidence_dict['conditionState'][service_time<=CS1_TO_CS2] = 1
+#evidence_dict['conditionState'][np.logical_and(service_time>CS1_TO_CS2, service_time<=CS2_TO_CS3)] = 2
+#evidence_dict['conditionState'][np.logical_and(service_time>CS2_TO_CS3, service_time<=CS3_TO_CS4)] = 3
+#evidence_dict['conditionState'][np.logical_and(service_time>CS3_TO_CS4, service_time<=EVIDENCE_END_YR)] = 4
 
 #evidence_dict['conditionState'][service_time==CS1_TO_CS2] = 2
 #evidence_dict['conditionState'][service_time==CS2_TO_CS3] = 3
